ebd46d23241b80ce32bd8d4b444fa73ef9280a05
MokiyCodes <mokiycodes@users.noreply.github.com>
2022-11-17 13:07:58 +0100

    test: :construction_worker: output the output

--- tests/index.js
+++ tests/index.js
@@ -4,7 +4,7 @@ let output = '';
 lib.instructions.outc = ()=>output += String.fromCharCode(lib.registers.r1);
 lib.execute(fs.readFileSync('./tests/hello-world.unasm', 'utf-8'));
 if (output !== 'Hello World!')
-  throw new Error('Hello World program did not output hello world');
+  throw new Error(`Hello World program did not output hello world\nOutputted:${output}`);
 else
   console.log('Hello World: Success');
 output = '';
